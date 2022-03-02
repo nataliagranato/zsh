eecf1876c8e82288271695aa9a577e8c1aca99e4
Natália Granato <91998391+nataliagranato@users.noreply.github.com>
2022-03-02 18:08:46 -0300

    Add files via upload
    Instalando o Zsh.

---
+++ sudo apt install zsh -y.sh
@@ -0,0 +1,7 @@
+sudo apt install zsh -y
+sudo apt-get install powerline fonts-powerline -y
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+
+
